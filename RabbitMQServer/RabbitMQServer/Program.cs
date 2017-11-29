2b91ca50b929c3f32da3bd6075d6833b70039cce
FreshMan <qinbocai@sina.cn>
2017-11-29 16:59:41 +0800

    Change demo code position.

--- RabbitMQServer/RabbitMQServer/Program.cs
+++ RabbitMQServer/RabbitMQServer/Program.cs
@@ -9,28 +9,28 @@ namespace RabbitMQServer
     {
         static void Main(string[] args)
         {
-            //var factory = new ConnectionFactory { HostName = "localhost" };
-            //using (var connection = factory.CreateConnection())
-            //{
-            //    using (var channel = connection.CreateModel())
-            //    {
-            //        var i = 50;
-            //        while (i>0)
-            //        {
-            //            channel.QueueDeclare("task_queue", true, false, false, null);
-            //            string message = i.ToString();
-            //            var body = Encoding.UTF8.GetBytes(message);
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    var i = 10000;
+                    while (i > 0)
+                    {
+                        channel.QueueDeclare("task_queue", true, false, false, null);
+                        string message = i.ToString();
+                        var body = Encoding.UTF8.GetBytes(message);
 
-            //            var properties = channel.CreateBasicProperties();
-            //            properties.Persistent = true;
+                        var properties = channel.CreateBasicProperties();
+                        properties.Persistent = true;
 
-            //            channel.BasicPublish("", "task_queue", properties, body);
-            //            Console.WriteLine($" Had send {message}");
-            //            i--;
-            //        }
+                        channel.BasicPublish("", "task_queue", properties, body);
+                        Console.WriteLine($" Had send {message}");
+                        i--;
+                    }
 
-            //    }
-            //}
+                }
+            }
 
             //var factory = new ConnectionFactory { HostName = "localhost" };
             //using (var connection = factory.CreateConnection())
@@ -66,20 +66,20 @@ namespace RabbitMQServer
             //    }
             //}
 
-            var factory = new ConnectionFactory { HostName = "localhost" };
-            using (var connection = factory.CreateConnection())
-            {
-                using (var channel = connection.CreateModel())
-                {
-                    args = new[] { "kern.critical", "A critial kernel error" };
-                    channel.ExchangeDeclare("topic_logs", "topic");
-                    var routingKey = (args.Length > 0) ? args[0] : "anonymous.info";
-                    string message = args.Length > 1 ? string.Join(" ", args.Skip(1).ToArray()) : "Hello Kety.";
-                    var body = Encoding.UTF8.GetBytes(message);
-                    channel.BasicPublish("topic_logs", routingKey, null, body);
-                    Console.WriteLine($" Had send {routingKey}:{message}");
-                }
-            }
+            //var factory = new ConnectionFactory { HostName = "localhost" };
+            //using (var connection = factory.CreateConnection())
+            //{
+            //    using (var channel = connection.CreateModel())
+            //    {
+            //        args = new[] { "kern.critical", "A critial kernel error" };
+            //        channel.ExchangeDeclare("topic_logs", "topic");
+            //        var routingKey = (args.Length > 0) ? args[0] : "anonymous.info";
+            //        string message = args.Length > 1 ? string.Join(" ", args.Skip(1).ToArray()) : "Hello Kety.";
+            //        var body = Encoding.UTF8.GetBytes(message);
+            //        channel.BasicPublish("topic_logs", routingKey, null, body);
+            //        Console.WriteLine($" Had send {routingKey}:{message}");
+            //    }
+            //}
             Console.ReadKey();
         }
     }
--- RabbitMQServerClient/RabbitMQServerClient/Program.cs
+++ RabbitMQServerClient/RabbitMQServerClient/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -13,31 +14,39 @@ namespace RabbitMQServerClient
     {
         static void Main(string[] args)
         {
-            //var factory = new ConnectionFactory { HostName = "localhost" };
-            //using (var connection = factory.CreateConnection())
-            //{
-            //    using (var channel = connection.CreateModel())
-            //    {
-            //        while (true)
-            //        {
-            //            channel.QueueDeclare("task_queue", true, false, false, null);
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    int i = 0;
+                    while (true)
+                    {
+                        channel.QueueDeclare("task_queue", true, false, false, null);
 
-            //            channel.BasicQos(0, 1, false);
-            //            Console.WriteLine("Waiting for message.");
-            //            var consumer = new EventingBasicConsumer(channel);
-            //            consumer.Received += (model, ea) =>
-            //            {
-            //                var body = ea.Body;
-            //                var message = Encoding.UTF8.GetString(body);
-            //                Console.WriteLine($"Received {message}");
-            //                channel.BasicAck(ea.DeliveryTag, false);
-            //            };
-            //            channel.BasicConsume("task_queue", false, consumer);
-
-            //            Thread.Sleep(3000);
-            //        }
-            //    }
-            //}
+                        channel.BasicQos(0, 1, false);
+                        //Console.WriteLine("Waiting for message.");
+                        var consumer = new EventingBasicConsumer(channel);
+                        consumer.Received += (model, ea) =>
+                        {
+                            var body = ea.Body;
+                            var message = Encoding.UTF8.GetString(body);
+                            Console.WriteLine($"Received {message}");
+                            FileStream fs = new FileStream(@"d:\xx.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                            StreamWriter sw = new StreamWriter(fs);
+                            sw.BaseStream.Seek(0, SeekOrigin.End);
+                            sw.WriteLine($"Received {message} \n");
+                            sw.Flush();
+                            sw.Close();
+                            fs.Close();
+                            channel.BasicAck(ea.DeliveryTag, false);
+                        };
+                        channel.BasicConsume("task_queue", false, consumer);
+                        i++;
+                        Thread.Sleep(300000);
+                    }
+                }
+            }
 
             //var factory = new ConnectionFactory { HostName = "localhost" };
             //using (var connection = factory.CreateConnection())
@@ -103,48 +112,48 @@ namespace RabbitMQServerClient
             //    }
             //}
 
-            var factory = new ConnectionFactory { HostName = "localhost" };
-            using (var connection = factory.CreateConnection())
-            {
-                using (var channel = connection.CreateModel())
-                {
-                    //while (true)
-                    //{
-                        channel.ExchangeDeclare("topic_logs", "topic");
+            //var factory = new ConnectionFactory { HostName = "localhost" };
+            //using (var connection = factory.CreateConnection())
+            //{
+            //    using (var channel = connection.CreateModel())
+            //    {
+            //        //while (true)
+            //        //{
+            //            channel.ExchangeDeclare("topic_logs", "topic");
 
-                        var queueName = channel.QueueDeclare().QueueName;
-                        //args = new[] { "#" };
-                        args = new[] { "kern.*" };
-                        //args = new[] { "*.critical" };
-                        //args = new[] { "kern.*", "*.critical" };
-                        if (args.Length < 1)
-                        {
-                            Console.Error.WriteLine("Usage: {0} [info] [warning] [error]",
-                                  Environment.GetCommandLineArgs()[0]);
-                            Console.WriteLine(" Press [enter] to exit.");
-                            Console.ReadLine();
-                            Environment.ExitCode = 1;
-                            return;
-                        }
-                        foreach (var severity in args)
-                        {
-                            channel.QueueBind(queueName, "topic_logs", severity);
-                        }
-                        Console.WriteLine("Waiting for logs.");
-                        var consumer = new EventingBasicConsumer(channel);
-                        consumer.Received += (model, ea) =>
-                        {
-                            var body = ea.Body;
-                            var message = Encoding.UTF8.GetString(body);
-                            var routingKey = ea.RoutingKey;
-                            Console.WriteLine($"Received {routingKey}:{message}");
-                        };
-                        channel.BasicConsume(queueName, true, consumer);
-                    //    Thread.Sleep(3000);
-                    //}
-                    Console.ReadKey();
-                }
-            }
+            //            var queueName = channel.QueueDeclare().QueueName;
+            //            //args = new[] { "#" };
+            //            args = new[] { "kern.*" };
+            //            //args = new[] { "*.critical" };
+            //            //args = new[] { "kern.*", "*.critical" };
+            //            if (args.Length < 1)
+            //            {
+            //                Console.Error.WriteLine("Usage: {0} [info] [warning] [error]",
+            //                      Environment.GetCommandLineArgs()[0]);
+            //                Console.WriteLine(" Press [enter] to exit.");
+            //                Console.ReadLine();
+            //                Environment.ExitCode = 1;
+            //                return;
+            //            }
+            //            foreach (var severity in args)
+            //            {
+            //                channel.QueueBind(queueName, "topic_logs", severity);
+            //            }
+            //            Console.WriteLine("Waiting for logs.");
+            //            var consumer = new EventingBasicConsumer(channel);
+            //            consumer.Received += (model, ea) =>
+            //            {
+            //                var body = ea.Body;
+            //                var message = Encoding.UTF8.GetString(body);
+            //                var routingKey = ea.RoutingKey;
+            //                Console.WriteLine($"Received {routingKey}:{message}");
+            //            };
+            //            channel.BasicConsume(queueName, true, consumer);
+            //        //    Thread.Sleep(3000);
+            //        //}
+            //        Console.ReadKey();
+            //    }
+            //}
         }
     }
 }
